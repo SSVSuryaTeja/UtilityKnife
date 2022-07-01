17c3055a58ed6728432ac285b8d434a27568cbb8
SSVSurya <yellassvsuryateja@gmail.com>
2022-07-01 11:49:56 -0500

    Added a New Class

---
+++ UtilityKnife/UtilityKnife/Convertors/Convertors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityKnife.Convertors 
+{
+    public class FahrenheitToCelsius
+    {
+
+        public double FahrenheitToCelsiusConverter(double FahrenheitTemp)
+        {
+            double _fahreneithTemp = 0.0;
+            double _celsius = 0.0;
+            return _celsius;
+        }
+    }
+}
